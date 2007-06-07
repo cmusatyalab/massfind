5d365ccabe56a989b021f6adce52478a8cd219da
Adam Goode <agoode@andrew.cmu.edu>
2007-06-07 14:58:18 -0400

    Towards search results.
    Do some fancy alpha stuff that we may abandon.
    Put in a border. Later, we will have results here. It is
    a normal JPanel, so should be easy?

--- src/edu/cmu/cs/diamond/rsna2007/SearchPanel.java
+++ src/edu/cmu/cs/diamond/rsna2007/SearchPanel.java
@@ -4,21 +4,26 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 public class SearchPanel extends JPanel {
 
     public SearchPanel() {
         setOpaque(false);
+
+        setBorder(BorderFactory.createTitledBorder(null, "Search Results",
+                TitledBorder.CENTER, TitledBorder.TOP, null, Color.WHITE));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         Graphics2D g2 = (Graphics2D) g;
         g2.setColor(new Color(0, 0, 0, 0.8f));
-        
+
         g2.fillRect(0, 0, getWidth(), getHeight());
     }
 }
